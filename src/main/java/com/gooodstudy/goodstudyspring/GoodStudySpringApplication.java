9dd8da90a40431ecef403995a4ff1944c1d6aed9
GUIBATISTA16 <guimcbatista@gmai.com>
2024-10-19 01:14:19 +0100

    First commit(i think)

---
+++ src/main/java/com/gooodstudy/goodstudyspring/GoodStudySpringApplication.java
@@ -0,0 +1,13 @@
+package com.gooodstudy.goodstudyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodStudySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoodStudySpringApplication.class, args);
+    }
+
+}
